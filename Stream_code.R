70dfb0598810e1e6549978b3ce13d9b3873b03a5
Sacha Jellinek <sachamj@rdl-t86dyl.desktop.cloud.unimelb.edu.au>
2024-02-05 15:09:57 +1100

    streams data

--- Stream_code.R
+++ Stream_code.R
@@ -7,8 +7,8 @@ library(purrr)
 
 # read in the input and snapped points data 
 vv_data <- readxl::read_xlsx('~/uomShare/wergProj/W12 - Revegetation/Instream_veg/VV_sites_forstream_snap_outputs_201123.xlsx',sheet=1)
-vv_slope <- readxl::read_xlsx('~/uomShare/wergProj/W12 - Revegetation/Instream_veg/Slope_extra.xlsx',sheet=1)
-vv_scores <- st_read("~/uomShare/wergProj/W12 - Revegetation/Instream_veg/Veg_Visions_2021_sites_middle/points_with_vv_fixed_sj_reprojxy.shp")
+#vv_slope <- readxl::read_xlsx('~/uomShare/wergProj/W12 - Revegetation/Instream_veg/Slope_extra.xlsx',sheet=1)
+vv_scores <- st_read("~/uomShare/wergProj/W12 - Revegetation/Instream_veg/Veg_Visions_2021_sites/points_with_vv_fixed_sj_reprojxy.shp")
 vv_scores2 <- vv_scores %>% dplyr::select(Site_ID, vv_A_struc, vv_B_richn, vv_E_regen, vv_D_patch, vv_C_instr, vv_overall, vv_vv_scor, xcoord, ycoord)
 vv_scores2 <- vv_scores2 %>% 
   rename("site_id" = "Site_ID")%>% 
@@ -17,21 +17,20 @@ vv_scores2 <- vv_scores2 %>%
 
 db_mwstr <- RPostgres::dbConnect(RPostgres::Postgres(), "mwstr", host = "localhost", port = 5432, user = "readonly", password = "reachcode_42")
 sites <- vv_data %>% dplyr::select(site_id, reach_v12, carea_km2, af_2022, slope_perc, meanq_mm)
-
+# Get variables from db_mwstr
 cat_env_var <- DBI::dbGetQuery(db_mwstr, "SELECT reach, ei_2022, af_2022, meanq_mm FROM cat_env")
-slope <- DBI::dbGetQuery(db_mwstr, "SELECT reach, slope_perc FROM subc_env")
-catch_area <- DBI::dbGetQuery(db_mwstr, "SELECT reach, carea_km2 FROM subcs")
+#slope <- DBI::dbGetQuery(db_mwstr, "SELECT reach, slope_perc FROM subc_env")
+#catch_area <- DBI::dbGetQuery(db_mwstr, "SELECT reach, carea_km2 FROM subcs")
+#slopes <- dplyr::left_join(vv_slope, slope, by=c("reach_v12" = "reach"))
+#sites_catcharea <- dplyr::left_join(sites, catch_area, by=c("reach_v12" = "reach"))
+#sites_slope <- dplyr::left_join(sites, slope, by=c("reach_v12" = "reach"))
 
-slopes <- dplyr::left_join(vv_slope, slope, by=c("reach_v12" = "reach"))
-sites4 <- dplyr::left_join(sites, catch_area, by=c("reach_v12" = "reach"))
-sites3 <- dplyr::left_join(sites, slope, by=c("reach_v12" = "reach"))
-sites2 <- dplyr::left_join(sites, cat_env_var, by=c("reach_v12" = "reach"))
-writexl::write_xlsx(sites2, "~/uomShare/wergProj/W12 - Revegetation/Instream_veg/VV_sites_envdata_201123.xlsx")
+vvsites_db_mwstr_var <- dplyr::left_join(sites, cat_env_var, by=c("reach_v12" = "reach"))
+writexl::write_xlsx(vvsites_db_mwstr_var, "~/uomShare/wergProj/W12 - Revegetation/Instream_veg/VV_sites_envdata_201123.xlsx")
+# vvsites_db_mwstr_var data without data gaps and including geology and xy coordinates
+instream_221123 <- st_read("~/uomShare/wergProj/W12 - Revegetation/Instream_veg/VV_sites_envdata_221123_finalssitesdata.shp")
 
-instream <- st_read("~/uomShare/wergProj/W12 - Revegetation/Instream_veg/VV_sites_envdata_221123_finalssitesdata.shp")
-str(instream)
-instream2 <- st_read("~/uomShare/wergProj/W12 - Revegetation/Instream_veg/VV_sites_envdata_221123_finalssitesdata.shp")
-str(instream2)
+# Correlation analysis
 correlation_dat <- instream %>% dplyr::select(carea_km2, ei_2022, af_2022, slope_perc, meanq_mm)
 str(correlation_dat)
 nc_df2 <- correlation_dat %>% st_drop_geometry()
@@ -39,58 +38,62 @@ res <- cor(nc_df2)
 round(res, 2)
 
 # Plotting variables to look at their distribution
-ggplot(data = instream) + 
+ggplot(data = instream_221123) + 
   geom_point(mapping = aes(x = meanq_mm, y = site_id))
-ggplot(data = instream) + 
+ggplot(data = instream_221123) + 
   geom_point(mapping = aes(x = carea_km2, y = site_id))
-ggplot(data = instream) + 
+ggplot(data = instream_221123) + 
   geom_point(mapping = aes(x = af_2022, y = site_id))
-ggplot(data = instream) + 
+ggplot(data = instream_221123) + 
   geom_point(mapping = aes(x = slope_perc, y = site_id))
-ggplot(data = instream) + 
+ggplot(data = instream_221123) + 
   geom_point(mapping = aes(x = ei_2022, y = site_id))
-ggplot(data = instream) + 
+ggplot(data = instream_221123) + 
   geom_point(mapping = aes(x = GMUT1DESC, y = site_id))
 
 # Categorising variables
-instream$area <- cut(instream$carea_km2,
+instream_221123$area <- cut(instream_221123$carea_km2,
                      breaks=c(0, 250, 1000, 4000),
                      labels=c('A', 'B', 'C'))
-instream$rain <- cut(instream$meanq_mm,
+instream_221123$rain <- cut(instream_221123$meanq_mm,
                      breaks=c(0, 125, 250, 500, 1000),
                      labels=c('A', 'B', 'C', 'D'))
-instream$forest <- cut(instream$af_2022,
+instream_221123$forest <- cut(instream_221123$af_2022,
                      breaks=c(-0.1, 0.5, 1),
                      labels=c('A', 'B'))
-instream$slope <- cut(instream$slope_perc,
+instream_221123$slope <- cut(instream_221123$slope_perc,
                        breaks=c(-0.1, 5, 15, 33),
                        labels=c('A', 'B', 'C'))
-instream$ei <- cut(instream$ei_2022,
+instream_221123$ei <- cut(instream_221123$ei_2022,
                       breaks=c(-0.1, 0.05, 0.1, 0.3, 0.6),
                       labels=c('A', 'B', 'C', 'D'))
 #writexl::write_xlsx(instream, "~/uomShare/wergProj/W12 - Revegetation/Instream_veg/instream_categories.xlsx")
 ## Choose sites using slice_sample. Maintaining steep slopes and removing Coastal areas
-cat <- instream %>% dplyr::select(site_id, ei, slope, forest, rain, area, GMUT1DESC)
-cat1<- cat %>% filter(!GMUT1DESC %in% c("Coast (C)"))
-cat1DF <- cat1 %>% st_drop_geometry()
-group1 <- cat1DF %>% tidyr::expand(slope, rain, area, forest, ei)
-group2 <- cat1DF %>% tidyr::expand(nesting(site_id, slope, rain, area, ei))
+cat <- instream_221123 %>% dplyr::select(site_id, ei, slope, forest, rain, area, GMUT1DESC)
+#remove sites on the coast (n = 5)
+nocoast<- cat %>% filter(!GMUT1DESC %in% c("Coast (C)"))
+instream_221123_df <- nocoast %>% st_drop_geometry()
+# grouping instream variables but removing attenuated forest cover - af_2022
+instream_221123_grp <- instream_221123_df %>% tidyr::expand(nesting(site_id, slope, rain, area, ei))
 ## Write groups of site combinations to excel for manual grouping using concatenate
-writexl::write_xlsx(group2, "~/uomShare/wergProj/W12 - Revegetation/Instream_veg/Site_ID_categories_a.xlsx")
-## 33 groups of combinatations excluding atenuated forest cover
-group3 <- group2 %>% 
+writexl::write_xlsx(instream_221123_grp, "~/uomShare/wergProj/W12 - Revegetation/Instream_veg/Site_ID_categories_a.xlsx")
+# Re-import Site_ID_categories_a.xlsx with combinations of categories
+site_cat <- readxl::read_xlsx('~/uomShare/wergProj/W12 - Revegetation/Instream_veg/Site_ID_categories.xlsx',sheet=1)
+## 33 groups of combinatations excluding attenuated forest cover
+combinations <- instream_221123_grp %>% 
   group_by(slope, rain, area, ei) %>% 
   count
-#Read in new dataset
-site_cat <- readxl::read_xlsx('~/uomShare/wergProj/W12 - Revegetation/Instream_veg/Site_ID_categories.xlsx',sheet=1)
-geo <- instream %>% dplyr::select(site_id, GMUT1DESC)
+
+#add veg visions scores and geology to site_cat 
+geo <- instream_221123 %>% dplyr::select(site_id, GMUT1DESC)
 geo2 <- geo %>% st_drop_geometry()
 vvscores <- vv_scores2 %>% st_drop_geometry()
 site_cat <- left_join(site_cat, geo2)
 site_cat <- left_join(site_cat, vvscores)
-#All groups with less than 4 sites and steep slopes. Coastal sites excluded and attenuated forest cover not included in groups
+# Keeping all groups with less than 4 sites and steep slopes. Coastal sites excluded and attenuated forest cover not included in groups
 target_1 <- c("AABB","ABAB","ACAB","ACBD","ACCB","ADBA","ADCA","BBAC","BCAD","CAAA","CDAA","CCAA")
 grp1 <- filter(site_cat, group %in% target_1)
+
 sites_AAAA <- filter(site_cat, group == "AAAA")
 grp2<- sites_AAAA %>% group_by(GMUT1DESC) %>% slice_sample(n = 3)
 sites_4 <- filter(site_cat, group == "AABC")
@@ -201,3 +204,23 @@ strat_propprob <- grts(
   aux_var = "ei_20221"
 )
 plot(strat_propprob, instream, key.width = lcm(3))
+
+##select samplable sites
+instream_sites300124 <- st_read("~/uomShare/wergProj/W12 - Revegetation/Instream_veg/Veg_Visions_2021_sites/finalsubset_vv_data131223_2.shp")
+# add stream reach to data
+sites2 <- sites %>% dplyr::select(site_id, reach_v12)
+instream_sites300124reach <- dplyr::left_join(instream_sites300124, sites2, by=c("site_id"))
+## table with 2 columns: reach and sampleable dataframe in db_mwstr
+dat <- dbGetQuery(db_mwstr, "SELECT reach, sampleable FROM streams")
+#add the sampleable 0/1 information to your final_sites spatial table
+samplable_sites050224 <- dplyr::left_join(instream_sites300124reach, dat, by=c("reach_v12" = "reach"))
+writexl::write_xlsx(samplable_sites050224, "~/uomShare/wergProj/W12 - Revegetation/Instream_veg/Veg_Visions_2021_sites/samplable_sites050224.xlsx")
+
+#samplable_streams <- sf::st_read(db_mwstr,
+      #                 query = "SELECT * FROM streams WHERE sampleable = 0;")
+#streams2 <- streams %>% dplyr::select(reach, sampleable)
+#streams2_ng <- streams2 %>% st_drop_geometry()
+#samplable <- dplyr::left_join(final_sites, streams2_ng, by=c("reach_v12" = "reach"))
+#writexl::write_xlsx(samplable, "~/uomShare/wergProj/W12 - Revegetation/Instream_veg/Veg_Visions_2021_sites_middle/Not_samplable_sites.xlsx")
+
+
